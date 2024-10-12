6755e54c21d5e3e094604fecfead745bffb40025
nickhealthy <alskadmlcraz1@gmail.com>
2024-10-12 13:38:02 +0900

    1. 예제 만들기 - 로그 추적기 V2 - 파라미터로 동기화 개발

---
+++ src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -0,0 +1,90 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 해당 클래스를 사용해서 실제 로그를 시작하고 종료할 수 있다.(로그추적기V2)
+ */
+@Slf4j
+@Component
+public class HelloTraceV2 {
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 로그를 시작한다.
+     * 로그 메시지를 파라미터로 받아서 시작 로그를 출력
+     * 응답 결과로 현재 로그의 상태인 `TraceStatus`를 반환한다.
+     *
+     * @param message
+     * @return TraceStatus
+     */
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+
+    /**
+     * V2에서 추가
+     * 기존 `TraceId` 에서 `createNextId()` 를 통해 다음 ID를 구한다.
+     *
+     * @param beforeTraceId
+     * @param message
+     * @return TraceStatus
+     */
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX, nextId.getLevel()) + message);
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+
+    /**
+     * 로그를 정상 종료한다. 정상 흐름에서 호출한다.
+     * 파라미터로 시작 로그의 상태(<code>TraceStatus</code>)를 전달 받는다.
+     *
+     * @param status
+     */
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    /**
+     * 로그를 예외 상황으로 종료한다. 예외가 발생할 때 호출한다.
+     * 파라미터로 시작 로그의 상태(<code>TraceStatus</code>)를 전달 받는다.
+     *
+     * @param status
+     */
+
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+    }
+}
