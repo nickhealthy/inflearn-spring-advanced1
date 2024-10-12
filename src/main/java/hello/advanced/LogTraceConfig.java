98dd6b05ad139fadef47e42069f90f90baa52334
nickhealthy <alskadmlcraz1@gmail.com>
2024-10-12 17:14:24 +0900

    2. 쓰레드 로컬 - ThreadLocal - 쓰레드 로컬 동기화 - 적용

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,7 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+//        return new FieldLogTrace();       // 동시성 문제 발생
+        return new ThreadLocalLogTrace();   // ThreadLocal 사용, 동시성 문제 해결
     }
 }
